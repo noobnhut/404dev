fafe494f7e8976bd87840eb97cca958d8b829ce8
noobnhut <nguyen.nhut.99.2017@gmail.com>
2024-01-22 15:01:22 +0700

    nhut-lan1

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # 404dev
+
+# admin
++ dashboard
++ user
++ tags
++ post
++ render_ytb
++ about_link
++ noti_system
+
+# client
++ home
++ post_detail
++ infor user
++ collection ytb
++ about_link 
+
+# CSDL 
++ mysql + sequelize
+# Backend
++ nodejs - expressjs
+# Frontend
++ vuejs 3 + Tailwindcss + Vite
+
+# Dự án nhỏ của 404NFP Team
---
+++ database.sql
@@ -0,0 +1,122 @@
+Table users {
+  id integer [primary key]
+  username varchar
+  password varchar
+  fullname varchar
+  email varchar
+  img_user varchar
+  disable_status boolean
+  created_at date
+}
+
+Table reset_password
+{
+   id integer [primary key]
+   id_user integer
+   keyword_token varchar
+   created_at date
+}
+Table posts
+{
+  id integer [primary key]
+  id_user integer
+  title varchar
+  content varchar
+  tag_id integer
+  created_at date
+}
+
+Table tags
+{
+  id integer [primary key]
+  name_tag varchar
+  active_status boolean
+  created_at date
+}
+
+Table img_posts
+{
+  id integer [primary key]
+  id_post integer
+  public_id varchar
+  url_img varchar
+  created_at date
+}
+
+Table comment_post
+{
+  id integer [primary key]
+  id_post integer
+  content varchar
+  created_at date
+}
+
+Table like_post
+{
+  id integer [primary key]
+  id_post integer
+  status boolean
+  created_at date
+}
+
+Table about_link
+{
+  id integer [primary key]
+  title varchar
+  content varchar
+  status enum // about or link or guild
+  created_at date
+}
+
+Table render_ytb
+{
+  id integer [primary key]
+  title varchar
+  content varchar
+  link_video varchar
+  created_at date
+}
+
+Table follow_user
+{
+  id integer [primary key]
+  id_user integer
+  status boolean
+  created_at date
+}
+
+Table noti_post 
+{
+  id integer [primary key]
+  id_user integer
+  id_post integer
+  content varchar
+  created_at date
+}
+
+Table noti_system
+{
+  id integer [primary key]
+  id_user integer
+  content varchar
+  created_at date
+}
+Ref: "users"."id" < "posts"."id_user"
+
+Ref: "tags"."id" < "posts"."tag_id"
+
+Ref: "posts"."id" < "like_post"."id_post"
+
+Ref: "posts"."id" < "comment_post"."id_post"
+
+Ref: "posts"."id" < "img_posts"."id_post"
+
+Ref: "users"."id" < "reset_password"."id_user"
+
+Ref: "users"."id" < "follow_user"."id_user"
+
+Ref: "users"."id" < "noti_post"."id_user"
+
+Ref: "posts"."id" < "noti_post"."id_post"
+
+Ref: "users"."id" < "noti_system"."id_user"
